df32cdb7df0acb4920b17a30cf7343fb321a238e
Matt <ymc006@ucsd.edu>
2019-09-25 21:41:14 -0700

    set up application directory structure

---
+++ controllers/wavves_controller.js
@@ -0,0 +1,26 @@
+// ** = MVP
+
+// HTML ROUTES
+// 
+// 
+// "/": POS Dashboard
+// GET: To load main dashboard **
+
+
+// API ROUTES
+// 
+// 
+// "/api/items": Returns a JSON of all items available for sale
+// GET: To populate POS Dash **
+
+// "/api/items/:id?": Returns a JSON of the item with the specified ID
+// GET: To retrieve a specific item information **
+// PUT: To modify a specific item's information
+// DELETE: To delete a specific item
+
+// "/api/sales": Returns a JSON of all sales made since the launch of the POS
+// GET: To populate sales page **
+// POST: To add a new transaction **
+
+// "/api/sales/:id?": Returns a JSON of the transaction with the specified ID 
+// GET: To retrieve a specific transaction information **
