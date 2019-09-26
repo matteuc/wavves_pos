e861dc3f59cd29acb30b968fe1c8f8e2baea9009
Matt <ymc006@ucsd.edu>
2019-09-25 22:06:57 -0700

    added blank model files

---
+++ models/mgt.js
@@ -0,0 +1,5 @@
+// Model for management 
+    // viewing sales
+    // adding items
+    // deleting items 
+    // modifying items
---
+++ models/pos.js
@@ -0,0 +1,4 @@
+// Model for POS (customer view)
+// Getting products 
+// Adding a new transaction
+// Retrieve a specific product's information
