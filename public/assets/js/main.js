7fd9d3eba4b92fed952881aceb0d1afe797bd07f
Matt <ymc006@ucsd.edu>
2019-09-27 15:49:27 -0700

    added adding products to cart functionality  (not tested)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,5 @@
-import { privateEncrypt } from "crypto";
-
 $(document).ready(function () {
-    var cart = [];
+    var cart = {};
     var total = 0;
     var total_div = $("#total");
 
@@ -39,12 +37,43 @@ $(document).ready(function () {
 
     // POS DASH
     $(document).on('click', ".product-block-pos", function () {
-        // Add product to cart variable
+        var product = {};
+        product.id = $(this).attr("data-id");
+        product.name = $(this).attr("data-name");
+        product.price = parseFloat($(this).attr("data-price"));
 
-        // Add product to cart on page
+        // Hide empty cart message
+        $("#empty-cart-msg").hide();
+
+        // Detect if the same product is already in the cart
+        if (cart[product.id]) {
+            // if so, simply edit quantity in stored variable
+            cart[product.id].quantity += 1;
+
+            // ... and on the page
+            var product_block = $(`#cart-content #product-id-${product.id}`);
+            var product_name = $(`<p class="text-center">${product.name}</p>`);
+            var product_total = $(`<p class="text-center">$${product.price} x ${cart[product.id].quantity}</p>`);
+
+            product_block.empty();
+            product_block.append(product_name).append(product_total);
+
+        } else {
+            // If not, add new product in stored variable
+            product.quantity = 1;
+            cart[product.id] = product;
+
+            // ... and add new product block on page
+            var product_block = $(`<div>`);
+            product_block.attr("id", `product-id-${product.id}`);
+            var product_name = $(`<p class="font-weight-bold text-center">${product.name}</p>`);
+            var product_total = $(`<p class="text-center">$${product.price} x ${cart[product.id].quantity}</p>`);
+
+            product_block.append(product_name).append(product_total);
+        }
 
         // Display new total
-        total += parseFloat($(this).attr("data-price"));
+        total += product.price;
         total_div.text(total);
     });
 
